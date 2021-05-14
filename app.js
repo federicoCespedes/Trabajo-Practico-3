012b0454700b9de8d86144a8e4114c2d12b07edd
federicocespedes <fgcespedes1995@gmail.com>
2021-05-14 03:08:53 -0300

    trabajo practico 3

---
+++ app.js
@@ -0,0 +1,50 @@
+
+const moduloTareas = require('./tareas/index');
+
+const process = require('process');
+
+const comando = process.argv[2];
+
+switch (comando) {
+
+
+   case 'listar':
+
+      moduloTareas.listarTareas();
+
+      break;
+
+   case 'filtrar':
+
+      moduloTareas.filtrarTareas();
+
+      break;
+
+   case 'agregar':
+
+      let titulo = process.argv[3];
+      if (!titulo) {
+         console.log('Se requiere un título');
+         break
+      }
+      let estado = process.argv[4];
+      moduloTareas.agregarTareas(titulo, estado)
+      break;
+
+   case 'borrar':                           //nos borra por defecto la última tarea asignada.
+
+      moduloTareas.borrarTarea();
+
+
+      break
+   case undefined:
+
+      console.log('Atención - Tienes que pasar una acción.');
+
+      break
+
+   default:
+
+      console.log('No entiendo que quieres hacer')
+}
+
---
+++ tareas/index.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const tareas = JSON.parse(fs.readFileSync('./db/tareas.json', 'utf-8'));
+module.exports = {
+
+    leerJson: () => JSON.parse(fs.readFileSync('./db/tareas.json', 'utf-8')),
+
+
+    listarTareas: function () {         // Agregar tareas, borrar tareas ,filtrar tareas, Total de tareas
+
+        let tareas = this.leerJson();
+
+
+        for (let i = 0; i < tareas.length; i++) {
+
+            console.log(tareas[i]);
+        };
+    },
+    agregarTareas: function (titulo, estado) {
+
+        let nuevaTarea = {
+            titulo,
+            estado
+        }
+        let tareas = this.leerJson();
+        tareas.push(nuevaTarea)
+        fs.writeFileSync('./db/tareas.json', JSON.stringify(tareas), 'utf-8')
+    },
+    filtrarTareas: function (filtro) {
+        let tareasFiltradas = tareas.filter(tarea => tarea.estado === filtro || tarea.titulo.includes(filtro));
+        return console.log(tareasFiltradas)
+    },
+    borrarTarea: function () {
+        tareas.pop()
+        this.guardarJson(tareas)
+
+        this.listarTareas()
+    },
+    guardarJson: function (tareas) {
+        fs.writeFileSync('./db/tareas.json', JSON.stringify(tareas), 'utf-8')
+    }
+
+    }
+
+
+
+    
+
+   
+   
+
+
+
+        
+
+
+
